a7ec343e950b9b7b8866d200952524632a8655fd
aleolvfer <aleolvfer@hotmail.com>
2020-05-14 02:47:50 -0300

    adicionando melhorias

--- js/deletaPaciente.js
+++ js/deletaPaciente.js
@@ -1,21 +1,16 @@
 function deletaPaciente(){
-    let tdBotao = document.createElement("td");
 
     let botao = document.createElement("button");
     botao.classList.add("botao-de-deleta");
-    botao.textContent = "X";
+    botao.textContent = "x";    
 
     botao.addEventListener("dblclick", function(event){
-        var deleta = event.target.parentNode;
-        deleta.parentNode.classList.add("deletando");
+        event.target.parentNode.classList.add("deletando");
 
         setTimeout(function(){
-            deleta.parentNode.remove();
-        }, 500);
-
-        
+            event.target.parentNode.remove();
+        }, 500); 
     });
 
-    tdBotao.appendChild(botao);
-    return tdBotao;
+    return botao;
 }
--- js/limpaForm.js
+++ js/limpaForm.js
@@ -1,27 +1,28 @@
 function clickLimpa(mome, peso, altura, gordura){
    
     let inputPeso = document.querySelector("#peso"); 
-    inputPeso.addEventListener("click", function(){
+    inputPeso.addEventListener("focus", function(){
         form.peso.value = "";
         form.peso.classList.remove("paciente-invalido");
     });
 
     let inputAltura = document.querySelector("#altura"); 
-    inputAltura.addEventListener("click", function(){
+    inputAltura.addEventListener("focus", function(){
         form.altura.value = "";
         form.altura.classList.remove("paciente-invalido");
     });
 
     let inputNome = document.querySelector("#nome"); 
-    inputNome.addEventListener("click", function(){
+    inputNome.addEventListener("focus", function(){
         form.nome.value = "";
         form.nome.classList.remove("paciente-invalido");
     });
 
     let inputGordura = document.querySelector("#gordura"); 
-    inputGordura.addEventListener("click", function(){
+    inputGordura.addEventListener("focus", function(){
         form.gordura.value = "";
         form.gordura.classList.remove("paciente-invalido");
-    });
+    }); 
+    
 
 }
